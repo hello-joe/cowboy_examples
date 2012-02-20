add5fa516b5f5257a71c76ff7c06304fcaeb843f
Loïc Hoguin <essen@dev-extend.eu>
2012-02-20 09:06:50 +0100

    Removed SASL output from the start.sh script, print URLs to examples instead

--- start.sh
+++ start.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-erl -sname cowboy_examples -pa ebin -pa deps/*/ebin \
-	-boot start_sasl -s cowboy_examples
+erl -sname cowboy_examples -pa ebin -pa deps/*/ebin -s cowboy_examples \
+	-eval "io:format(\"~n~nThe following examples are available:~n\")." \
+	-eval "io:format(\"* Hello world: http://localhost:8080~n\")." \
+	-eval "io:format(\"* Websockets: http://localhost:8080/websocket~n\")." \
+	-eval "io:format(\"* Eventsource: http://localhost:8080/eventsource~n\")."
